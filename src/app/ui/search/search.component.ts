77671ae9de20324e5ee5d298789754440c378b7d
AntonReshetovv <reshetov190991@mail.ru>
2025-07-08 23:13:44 +0700

     Изменено: Изменена логика работы поиска, использован formControl

--- src/app/ui/search/search.component.ts
+++ src/app/ui/search/search.component.ts
@@ -1,17 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatInputModule } from '@angular/material/input'
-import { MatIconModule } from '@angular/material/icon'
-import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import {ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {debounceTime, distinctUntilChanged, Subject, takeUntil} from 'rxjs';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
   standalone: true,
-  imports: [MatInputModule, MatIconModule, FormsModule, NgIf],
+  imports: [MatInputModule, MatIconModule, ReactiveFormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchComponent {
-  value = ''
+export class SearchComponent implements OnInit, OnDestroy {
+  @Output() searchChanged = new EventEmitter<string>();
+  searchControl = new FormControl('')
+
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe(value => {
+      this.searchChanged.emit(value?.toLowerCase() ?? '')
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
