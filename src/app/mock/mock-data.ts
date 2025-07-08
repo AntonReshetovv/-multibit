d02f51660b4db877d309fef02bdd40e5d0179ada
AntonReshetovv <reshetov190991@mail.ru>
2025-07-08 23:15:13 +0700

    Добавлено: Подготовлены моковые данные

---
+++ src/app/mock/mock-data.ts
@@ -0,0 +1,81 @@
+import { Task } from "../models/task";
+
+export const mockData: Task[] =[
+        {
+          id: "323fefwefew",
+          title: 'Сходить в бассейн',
+          status: false,
+        },
+        {
+          id: "323fefwf44343r34",
+          title: 'Сходить на море',
+          description: 'Предложили сходить на косу, там сегодня водичка прозрачная и теплая. На пляже находится воллейбол, а также зона барбекю ))',
+          status: false,
+        },
+        {
+          id: "323",
+          title: 'Купить лимонад',
+          description: 'Сегодня в жаркий день я предпочитаю ТАРХУН!!!!',
+          status: false,
+        },
+        {
+          id: "302402-4атлуауа",
+          title: 'Почитать хабр',
+          description: 'Статейки по Ангулярчику вышли, нужно чекнуть )',
+          status: false,
+        },
+        {
+          id: "ащ32щщ24щщ4239931391-23",
+          title: 'Спеть песню',
+          description: 'In your head Zombie!!!  In your head Zombie!!!  In your head Zombie!!!  In your head Zombie!!!  In your head Zombie!!!',
+          status: false,
+        },
+        {
+          id: "fejfwnfnfe233",
+          title: 'Сходить на лыжи',
+          description: 'Снег сегодня мягкий, планирую 5 км пробежать )',
+          status: false,
+        },
+        {
+          id: "323fefwf44332r3tr434234",
+          title: 'Погладить кота',
+          description: 'Котенок маленький, требует ласки, нужно не забыть его по чесать )))',
+          status: false,
+        },
+        {
+          id: "111123424ауа323",
+          title: 'Сделать дома ремонт',
+          description: 'Балкон, кухню да вообще можно все по новой делать ))',
+          status: false,
+        },
+        {
+          id: "323fefwf44332r3tr43яяя",
+          title: 'Покушать',
+          description: 'Профит есть - можно поесть ))) сегодня у меня сосиски с лапшой ))',
+          status: false,
+        },
+        {
+          id: "323fefwf44332r3tr43ауа",
+          title: 'Не забыть сходить за хлебом',
+          description: 'Вспомнил кореша своего, у него татуха на руки с такой надписью )))',
+          status: false,
+        },
+        {
+          id: "323fefwf44332r3tr4332342",
+          title: 'Купить молоко',
+          description: 'завод Ирмень',
+          status: false,
+        },
+        {
+          id: "323fefwf44332r3tr43ддда",
+          title: 'Встреча с коллегами',
+          description: 'Обсудить задачу, помочь найти решение! Главное команда!',
+          status: false,
+        },
+        {
+          id: "323fefwf44332r3tr43яажуалжуа",
+          title: 'Выйти погулять',
+          description: 'сегодня цели пройти минимум 10км',
+          status: false,
+        }
+]
