704ee7a8c834d09d6a7956da59cf3b06a4b84bb3
AntonReshetovv <reshetov190991@mail.ru>
2025-07-08 23:20:11 +0700

    Добавлено: Отображение детальной информации задачи

--- src/app/pages/task-details/task-details.component.ts
+++ src/app/pages/task-details/task-details.component.ts
@@ -1,15 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { TaskItemComponent } from "src/app/ui/task-item/task-item.component";
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+
+import {Task} from 'src/app/models/task';
+import {TaskService} from 'src/app/services/task.service';
+import {TaskItemComponent} from "src/app/ui/list/task-item/task-item.component";
 
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.scss'],
   standalone: true,
-  imports: [RouterLink, TaskItemComponent],
+  imports: [TaskItemComponent, NgIf],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaskDetailsComponent {
-    constructor() { }
+export class TaskDetailsComponent implements OnInit {
+  task: Task | undefined
+
+  constructor(
+    private taskService: TaskService,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    const taskId = this.activatedRoute.snapshot.paramMap.get('id')
+    if (taskId) {
+      this.task = this.taskService.getById(taskId)
+    }
+  }
 }
