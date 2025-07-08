36af0a925763cf983d5cafec67b01e8007b0c52b
AntonReshetovv <reshetov190991@mail.ru>
2025-07-08 11:21:41 +0700

    Добавлено: Верстка ui компонентов (форма, список, поиск, элемент списка)

--- src/app/ui/search/search.component.ts
+++ src/app/ui/search/search.component.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIconModule, FormsModule, NgIf],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchComponent {
-
+  value = ''
 }
--- src/app/ui/task-list/task-list.component.ts
+++ src/app/ui/task-list/task-list.component.ts
@@ -1,13 +1,25 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [MatListModule, NgFor, MatButtonModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskListComponent {
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 
+  onReadMore(event: Event) {
+    event.stopPropagation()
+  }
+
+  onRemove(event: Event) {
+    event.stopPropagation()
+  }
 }
+
