8ef1bb69ca73d5f2a807e559b533ab12840da5b8
AntonReshetovv <reshetov190991@mail.ru>
2025-07-08 23:17:17 +0700

    Добавлено: Логика работы формы

---
+++ src/app/ui/form/add-task-form/add-task-form.component.ts
@@ -0,0 +1,51 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogRef} from '@angular/material/dialog';
+
+import {TaskService} from 'src/app/services/task.service';
+
+
+@Component({
+  selector: 'app-add-task-form',
+  templateUrl: './add-task-form.component.html',
+  styleUrls: ['./add-task-form.component.scss'],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AddTaskFormComponent {
+
+  form = new FormGroup<{
+    title: FormControl<string>,
+    description: FormControl<string>
+  }>({
+    title: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+    description: new FormControl('', {nonNullable: true})
+  })
+
+  constructor(
+    private taskService: TaskService,
+    public dialogRef: MatDialogRef<AddTaskFormComponent>
+  ) {
+  }
+
+  addTask() {
+    const {title, description} = this.form.getRawValue()
+    if (title.trim()) {
+      this.taskService.create({
+        title,
+        description,
+        status: false
+      })
+      this.dialogRef.close()
+    }
+  }
+}
