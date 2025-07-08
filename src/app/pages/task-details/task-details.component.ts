d65cdcb56e9b165a0407c519ee7ed37ea3b25378
AntonReshetovv <reshetov190991@mail.ru>
2025-07-08 11:55:58 +0700

    Добавлено: Кнопка навигации "Назад" и отображение детальной информации задачи

--- src/app/pages/task-details/task-details.component.ts
+++ src/app/pages/task-details/task-details.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TaskItemComponent } from "src/app/ui/task-item/task-item.component";
 
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [RouterLink, TaskItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskDetailsComponent {
-
+    constructor() { }
 }
